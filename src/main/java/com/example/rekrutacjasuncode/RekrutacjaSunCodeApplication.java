76bc3640348756b7001e9d8ce120cb56fa9f68c4
Jakub Czajka <CzajkaPL@o2.pl>
2022-04-08 16:05:41 +0200

    Init commit

---
+++ src/main/java/com/example/rekrutacjasuncode/RekrutacjaSunCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rekrutacjasuncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekrutacjaSunCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RekrutacjaSunCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rekrutacjasuncode/RekrutacjaSunCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rekrutacjasuncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekrutacjaSunCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
