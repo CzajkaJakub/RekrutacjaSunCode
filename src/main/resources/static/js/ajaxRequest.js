966609a2ee69058ffb2a25418f2a76ababc940b5
Jakub Czajka <CzajkaPL@o2.pl>
2022-04-09 16:00:39 +0200

    Added data table

--- src/main/resources/static/js/ajaxRequest.js
+++ src/main/resources/static/js/ajaxRequest.js
@@ -1,9 +1,31 @@
 function loadTable() {
     let request = new XMLHttpRequest();
-    console.log("XD")
     request.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("table").innerHTML = this.responseText;
+
+            let parsedJSON = JSON.parse(this.responseText);
+
+            let htmlTable =`<table class="container" id="table"><thead><tr>
+                                <th scope="col"><h1>Id</h1></th>
+                                <th scope="col"><h1>Kolumna1</h1></th>
+                                <th scope="col"><h1>Kolumna2</h1></th>
+                                <th scope="col"><h1>Kolumna3</h1></th>
+                                <th scope="col"><h1>Kolumna4</h1></th>
+                            </tr></thead><tbody>`
+
+            for (let i = 0; i < parsedJSON.length; i++) {
+                htmlTable = htmlTable + "<tr>" +
+                    "<td>" + parsedJSON[i].id + "</td>" +
+                    "<td>" + parsedJSON[i].kolumna1 + "</td>" +
+                    "<td>" + parsedJSON[i].kolumna2 + "</td>" +
+                    "<td>" + parsedJSON[i].kolumna3 + "</td>" +
+                    "<td>" + parsedJSON[i].kolumna4 + "</td>" +
+                    "</tr>"
+            }
+
+            htmlTable = htmlTable +"</tbody></table>";
+            console.log(htmlTable)
+            document.getElementsByClassName("content")[0].innerHTML = htmlTable;
         }
     };
 
